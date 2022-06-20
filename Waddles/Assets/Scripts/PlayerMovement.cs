413d5cec6b01fcbf60e12f28ca60e36ca91e8c65
Leasky02 <adleask@hotmail.co.uk>
2022-06-20 22:52:45 +0100

    Project Created and Ragdoll implemented

---
+++ Waddles/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private ConfigurableJoint cj;
+
+    [SerializeField] private int speed;
+    [SerializeField] private int rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cj = GetComponent<ConfigurableJoint>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //get input direction and move
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+        rb.AddForce(movementDirection * speed * Time.deltaTime);
+
+        if(movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            cj.targetRotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed);
+        }
+    }
+}
